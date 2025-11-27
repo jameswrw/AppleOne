1e59b9d8763d4fdc43ec78d9b2d5d686c1a9686b
James Weatherley <james@weatherley.net>
2025-11-27 21:46:56 +0000

    Get backspace working

--- AppleOne/AppleOneViewModel.swift
+++ AppleOne/AppleOneViewModel.swift
@@ -24,22 +24,35 @@ public final class AppleOneViewModel: ObservableObject {
     
     func outputCharacterHandler(_ key: UInt8) {
         var output = key
+        
+        // The Apple 1 uses 0x0D (CR) for newline, so translate to 0x0A (NL) for the host.
         if output == 0x0D {
             output = 0x0A
         }
         
         if output == 0x0A || (0x20..<0x7F).contains(output) {
-            text.append(String(UnicodeScalar(output)))
-        } else if output == 0x7F {
-            let _ = text.popLast()
+            // WozMon uses 0x5F '_' for backspace.
+            if output == 0x5F {
+                let _ = text.popLast()
+            } else {
+                text.append(String(UnicodeScalar(output)))
+            }
         }
     }
     
     func keyPressed(_ key: String) {
         // Ignore non-ASCII
         if key.count == 1, var asciiValue = key.uppercased().unicodeScalars.first?.value, asciiValue < 0x80 {
+            // The Apple 1 uses 0x0D (CR) for newline, so translate from 0x0A (NL) provided by the host.
+            //
+            // Strangely, it uses 0x5F ('_') for backspace, so translate from the host which provides 0x7F.
+            // This could be a mistake from Woz, interpreting a terminal rubout causing '_' to be displayed,
+            // so WozMon looks for a literal 0x5F, rather than 0x7F which would have generated the '_' as
+            // a placeholder for 0x7F.
             if asciiValue == 0x0A {
                 asciiValue = 0x0D
+            } else if asciiValue == 0x7F {
+                asciiValue = 0x5F
             }
             appleOne?.inputCharacter(UInt8(asciiValue))
         }
